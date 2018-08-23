8b6657aab41e5883cf3d406ee824e10c76f060d2
habin-isa <habin@hotmail.co.uk>
2018-08-23 14:42:32 +0100

    Creates a basic news feed single page application using a news API

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <section class="hero has-text-centered is-primary">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">PWA News</h1>
+                    <h2 class="subtitle">All the headlines making the wavy!</h2>
+                </div>
+            </div>
+        </section>
+        <nuxt/>
+    </div>
+</template>
+
+<style>
+    html {
+        font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="section">
+        <div class="container">
+            <div class="columns is-multiline">
+                <div
+                    class="column is-one-quarter"
+                    v-for="(article, index) in articles"
+                    :key="index">
+                        <a :href="article.url" target="_blank">
+                            <div class="card">
+                                <div class="card-image">
+                                    <figure class="image is-3by2">
+                                        <img :src="article.urlToImage" :alt="article.title">
+                                    </figure>
+                                </div>
+                                <div class="card-content">
+                                    <div class="content">{{ article.title }}</div>
+                                </div>
+                            </div>
+                        </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        async asyncData({ app }) {
+            const { articles } = await app.$axios.$get(
+                `https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=${
+                    process.env.API_KEY
+                }`
+            );
+
+            return { articles };
+        },
+    };
+</script>
